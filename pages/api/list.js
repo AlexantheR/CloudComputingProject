5bd1f825a6ffcbd4e0be2ddb60fd6306b462b8bc
AlexantheR <adinu90@gmail.com>
2024-05-07 19:39:34 +0300

    final commit maybe?

--- pages/api/list.js
+++ pages/api/list.js
@@ -1,22 +1,92 @@
 // import { connectToDatabase } from "../../lib/connectToDatabase";
 import clientPromise from "../../lib/mongodb";
-
+import { ObjectId } from "bson";
 export default async function handler(req, res) {
+  // try {
+  //   const mongoClient  = await clientPromise;
+  //   const db = mongoClient.db("sample_restaurants");
+  //   const collection = db.collection("restaurants");
+  //   const results = await collection
+  //     .find({})
+  //     .project({
+  //       grades: 0,
+  //       borough: 0,
+  //       restaurant_id: 0,
+  //     })
+  //     .limit(10)
+  //     .toArray();
+  //   res.status(200).json(results);
+  // } catch (e) {
+  //   console.error(e);
+  // }
+
   try {
-    const mongoClient  = await clientPromise;
-    const db = mongoClient.db("sample_restaurants");
-    const collection = db.collection("restaurants");
-    const results = await collection
-      .find({})
-      .project({
-        grades: 0,
-        borough: 0,
-        restaurant_id: 0,
-      })
-      .limit(10)
-      .toArray();
-    res.status(200).json(results);
+    if (req.method === "POST") {
+      const mongoClient = await clientPromise;
+      const db = mongoClient.db("bookStoreDatabase");
+      try {
+        const collection = db.collection('Bookstore');
+        const book = await collection.insertOne(req.body);
+        res.status(201).json({ success: true, data: book });
+
+      } catch (error) {
+        res.status(400).json({ success: false, error: "Failed to add Book" });
+        console.error("Error adding Book:", error);
+      }
+    } else if (req.method === "GET") {
+      const mongoClient = await clientPromise;
+      const db = mongoClient.db("bookStoreDatabase");
+
+      const collection = db.collection("Bookstore");
+
+      const results = await collection
+        .find({})
+        .project({})
+        .limit(10)
+        .toArray();
+      res.status(200).json(results);
+    } else if (req.method === "DELETE") {
+      const mongoClient = await clientPromise;
+      const db = mongoClient.db("bookStoreDatabase");
+      const collection = db.collection("Bookstore");
+      const { id } = req.query;
+      try {
+        const deletedBook = await collection.deleteOne({ _id: new ObjectId(id) });
+        if (deletedBook.deletedCount === 1) {
+          res.status(200).json({ success: true, message: "Book deleted successfully" });
+        } else {
+          res.status(404).json({ success: false, error: "Book not found" });
+          console.log(id);
+        }
+      } catch (error) {
+        res.status(500).json({ success: false, error: "Internal server error" });
+        console.error("Error deleting Book:", error);
+      }
+    } else if (req.method === "PUT" || req.method === "PATCH") {
+      const mongoClient = await clientPromise;
+      const db = mongoClient.db("bookStoreDatabase");
+      const collection = db.collection("Bookstore");
+      const { id } = req.query;
+      try {
+        const updatedBook = await collection.updateOne(
+          { _id: new ObjectId(id) },
+          { $set: req.body }
+        );
+        if (updatedBook.matchedCount === 1) {
+          res.status(200).json({ success: true, message: "Book updated successfully" });
+        } else {
+          res.status(404).json({ success: false, error: "Book not found" });
+        }
+      } catch (error) {
+        res.status(500).json({ success: false, error: "Internal server error" });
+        console.error("Error updating Book:", error);
+      }
+    }
+    else {
+      res.status(405).json({ success: false, error: "Method Not Allowed" });
+    }
   } catch (e) {
     console.error(e);
+    res.status(500).json({ error: "Internal server error" });
   }
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -35,15 +35,152 @@ export const getServerSideProps: GetServerSideProps<
 export default function Home({
   isConnected,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) {
-  const [restaurants, setRestaurants] = useState<any[]>([]);
+  const [books, setBooks] = useState<any[]>([]);
+
+  const [editingBookId, setEditingBookId] = useState<string | null>(null);
+  const [updatedBookData, setUpdatedBookData] = useState({
+    title: "",
+    author: "",
+    genre: "",
+    numPages: ""
+  });
+
+
+  const [formData, setFormData] = useState({
+    title: "",
+    author: "",
+    genre: "",
+    numPages: ""
+  });
+
+  const handleChange = (e: any) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value
+    });
+  };
+
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch("/api/list", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(formData)
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        console.log("New book added:", data);
+        setBooks([...books, data]);
+        setFormData({ title: "", author: "", genre: "", numPages: "" });
+        fetchBooks();
+      } else {
+        console.error("Failed to add book");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
+  const handleDelete = async (id: string) => {
+    const isConfirmed = window.confirm('Are you sure you want to delete this book?');
+    if (!isConfirmed) {
+      return; // Do nothing if the user cancels
+    }
+
+    try {
+      const response = await fetch(`/api/list?id=${id}`, {
+        method: "DELETE"
+      });
+
+      if (response.ok) {
+        console.log("Book deleted successfully");
+        fetchBooks();
+      } else {
+        console.error("Failed to delete book");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
+  const handleUpdate = (id: string) => {
+    setEditingBookId(id);
+    // Retrieve the book data corresponding to the id and set it in updatedBookData state
+    const bookToUpdate = books.find(book => book._id === id);
+    if (bookToUpdate) {
+      setUpdatedBookData({
+        title: bookToUpdate.title,
+        author: bookToUpdate.author,
+        genre: bookToUpdate.genre,
+        numPages: bookToUpdate.numPages
+      });
+    }
+  };
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setUpdatedBookData({
+      ...updatedBookData,
+      [name]: value
+    });
+  };
+
+  const handleSave = async (id: string) => {
+    try {
+      const response = await fetch(`/api/list?id=${id}`, {
+        method: "PUT", // or "PATCH" depending on your API
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(updatedBookData)
+      });
+
+      if (response.ok) {
+        console.log("Book updated successfully");
+        fetchBooks();
+        setEditingBookId(null); // Reset editing mode
+      } else {
+        console.error("Failed to update book");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
+  const handleCancel = () => {
+    setEditingBookId(null); // Exit editing mode
+    // Reset updatedBookData to original book data
+    const originalBookData = books.find(book => book._id === editingBookId);
+    if (originalBookData) {
+      setUpdatedBookData({
+        title: originalBookData.title,
+        author: originalBookData.author,
+        genre: originalBookData.genre,
+        numPages: originalBookData.numPages
+      });
+    }
+  };
 
   useEffect(() => {
-    (async () => {
-      const results = await fetch("/api/list");
-      const resultsJson = await results.json();
-      setRestaurants(resultsJson);
-    })();
+    fetchBooks();
   }, []);
+
+  const fetchBooks = async () => {
+    try {
+      const response = await fetch("/api/list");
+      const booksData = await response.json();
+      setBooks(booksData);
+      console.log(booksData);
+    } catch (error) {
+      console.error("Error fetching books:", error);
+    }
+  };
 
   return (
     <div className="container">
@@ -53,17 +190,127 @@ export default function Home({
       </Head>
 
       <main>
-       <h1 className="title">
-        Welcome to <a href="https://nextjs.org">Next.js with MongoDB!</a>
-       </h1>
-       <div className="grid">
-        {restaurants.map((restaurant) => (
-          <div className="card" key={restaurant._id}>
-            <h2>{restaurant.name}</h2>
-            <p>{restaurant.cuisine}</p>
-          </div>
-        ))}
-       </div>
+        <h1 className="title">
+          Bookstore cloud project
+        </h1>
+        <h2>
+          DINU Alexandru Cristian 1131
+        </h2>
+        <h1>
+
+          <form onSubmit={handleSubmit}>
+            <input
+              type="text"
+              name="title"
+              value={formData.title}
+              onChange={handleChange}
+              placeholder="Book Title"
+              required
+              pattern=".{3,}" // Minimum 3 characters
+              title="Please enter at least 3 characters"
+              style={{ marginRight: '0.5rem' }}
+            />
+            <input
+              type="text"
+              name="author"
+              value={formData.author}
+              onChange={handleChange}
+              placeholder="Book Author"
+              required
+              pattern=".{3,}" // Minimum 3 characters
+              title="Please enter at least 3 characters"
+              style={{ marginRight: '0.5rem' }}
+            />
+            <input
+              type="text"
+              name="genre"
+              value={formData.genre}
+              onChange={handleChange}
+              placeholder="Book Genre"
+              required
+              pattern=".{3,}" // Minimum 3 characters
+              title="Please enter at least 3 characters"
+              style={{ marginRight: '0.5rem' }}
+            />
+            <input
+              type="number"
+              name="numPages"
+              value={formData.numPages}
+              onChange={handleChange}
+              placeholder="Number of pages"
+              required
+              min="1" // Minimum value
+              title="Please enter a valid number"
+              style={{ marginRight: '0.5rem' }}
+            />
+            <button type="submit">Add to bookstore.</button>
+          </form>
+
+
+        </h1>
+        <div className="grid">
+          {books.map((book) => (
+            <div className="card" key={book._id}>
+              {editingBookId === book._id ? (
+                <form onSubmit={() => handleSave(book._id)}>
+                  <input
+                    type="text"
+                    name="title"
+                    value={updatedBookData.title}
+                    onChange={handleInputChange}
+                    placeholder="Book Title"
+                    required
+                    pattern=".{3,}" // Minimum 3 characters
+                    title="Please enter at least 3 characters"
+                  />
+                  <input
+                    type="text"
+                    name="author"
+                    value={updatedBookData.author}
+                    onChange={handleInputChange}
+                    placeholder="Book Author"
+                    required
+                    pattern=".{3,}" // Minimum 3 characters
+                    title="Please enter at least 3 characters"
+                  />
+                  <input
+                    type="text"
+                    name="genre"
+                    value={updatedBookData.genre}
+                    onChange={handleInputChange}
+                    placeholder="Book Genre"
+                    required
+                    pattern=".{3,}" // Minimum 3 characters
+                    title="Please enter at least 3 characters"
+                  />
+                  <input
+                    type="number"
+                    name="numPages"
+                    value={updatedBookData.numPages}
+                    onChange={handleInputChange}
+                    placeholder="Number of pages"
+                    required
+                    min="1" // Minimum value
+                    title="Please enter a valid number"
+                  />
+                  <button style={{ marginRight: '0.5rem' }} type="submit">Save</button>
+                  <button type="button" onClick={handleCancel}>Cancel</button>
+                </form>
+              ) : (
+                <>
+                  <h2>{book.title}</h2>
+                  <p>{book.author}</p>
+                  <p>{book.genre}</p>
+                  <p>{book.numPages}</p>
+                  <button style={{ marginRight: '0.5rem' }} onClick={() => handleUpdate(book._id)}>Update</button>
+                  <button onClick={() => handleDelete(book._id)}>Delete</button>
+                </>
+              )}
+            </div>
+          ))}
+        </div>
+
+
       </main>
 
       <footer>
@@ -78,153 +325,121 @@ export default function Home({
       </footer>
 
       <style jsx>{`
-        .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
+  .container {
+    min-height: 100vh;
+    padding: 0 1rem;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
 
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
+  main {
+    padding: 2rem;
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
 
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
+  footer {
+    width: 100%;
+    height: 100px;
+    border-top: 1px solid #ccc;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
 
-        footer img {
-          margin-left: 0.5rem;
-        }
+  footer img {
+    margin-left: 0.5rem;
+  }
 
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
+  a {
+    color: #333;
+    text-decoration: none;
+  }
 
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
+  .title {
+    margin: 0;
+    line-height: 1.15;
+    font-size: 3rem;
+    font-weight: bold;
+    text-align: center;
+  }
 
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
+  .grid {
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+    gap: 1rem;
+    max-width: 1200px;
+    margin-top: 2rem;
+  }
 
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
+  .card {
+    padding: 1.5rem;
+    border: 1px solid #ccc;
+    border-radius: 10px;
+    transition: transform 0.2s ease, box-shadow 0.3s ease; /* Added box-shadow transition */
+  }
 
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
+  .card:hover {
+    transform: translateY(-5px);
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1); /* Added box-shadow effect on hover */
+  }
 
-        .title,
-        .description {
-          text-align: center;
-        }
+  .card h2 {
+    margin-bottom: 0.5rem;
+    font-size: 1.5rem;
+    font-weight: bold;
+  }
 
-        .subtitle {
-          font-size: 2rem;
-        }
+  .card p {
+    margin: 0;
+    font-size: 1.2rem;
+    color: #555;
+  }
 
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
+  .card button {
+    margin-top: 1rem;
+    padding: 0.5rem 1rem;
+    background-color: #007bff;
+    color: #fff;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+    transition: background-color 0.3s ease;
+  }
 
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
+  .card button:hover {
+    background-color: #0056b3;
+  }
 
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-
-          max-width: 800px;
-          margin-top: 3rem;
-        }
-
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
-
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
-
-        .logo {
-          height: 1em;
-        }
-
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
-          }
-        }
+  @media (max-width: 768px) {
+    .grid {
+      grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
+    }
+  }
       `}</style>
 
       <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
+  html,
+  body {
+    padding: 0;
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+    background-color: #f7f7f7;
+    color: #333;
+  }
 
-        * {
-          box-sizing: border-box;
-        }
+  * {
+    box-sizing: border-box;
+  }
+
+  button:focus {
+    outline: none;
+  }
       `}</style>
     </div>
   );
